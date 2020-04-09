fcfc7651146ab574199f865541cad24b1bc7b6b8
NancyBolstad <litchacha@gmail.com>
2020-04-09 16:32:22 +0200

    Versioning api

--- backend/lib/server.js
+++ backend/lib/server.js
@@ -1,27 +1,26 @@
-'strict mode';
+"strict mode";
 
-var serveStatic = require('serve-static');
-var express = require('express');
-var path = require('path');
-var cors = require('cors');
-var data = require('../data/projects.json');
+var serveStatic = require("serve-static");
+var express = require("express");
+var path = require("path");
+var cors = require("cors");
+var data = require("../data/projects.json");
 
 var app = express();
 var port = process.env.PORT || 8001;
 var corsOptions = {
-    origin: '*',
-    credentials: true
-  };
-
+  origin: "*",
+  credentials: true
+};
 
 app.use(cors(corsOptions));
 
-app.use('/', serveStatic(path.resolve(__dirname, '../static/')));
+app.use("/", serveStatic(path.resolve(__dirname, "../frontend/dist")));
 
-app.get('/projects', function (_, res, _,) {
-    res.json(data);
+app.get("/v1/projects", function(_, res, _) {
+  res.json(data);
 });
 
 app.listen(port, () => {
-    console.log(`🚀Server listening on port http://localhost:${port}`);
-  });
+  console.log(`🚀Server listening on port http://localhost:${port}`);
+});
