ea52480022ae659e1543492b96a5acc4b988fe08
NancyBolstad <litchacha@gmail.com>
2020-04-11 12:54:44 +0200

    Test format

--- backend/lib/server.js
+++ backend/lib/server.js
@@ -1,54 +1,56 @@
-"strict mode";
+'strict mode'
 
-var serveStatic = require("serve-static");
-var express = require("express");
-var path = require("path");
-var cors = require("cors");
-var data = require("../data/projects.json");
+var serveStatic = require('serve-static')
+var express = require('express')
+var path = require('path')
+var cors = require('cors')
+var data = require('../data/projects.json')
 
-var app = express();
-var port = process.env.PORT || 8001;
+var app = express()
+var port = process.env.PORT || 8001
 var corsOptions = {
-  origin: "*",
-  credentials: true
-};
+  origin: '*',
+  credentials: true,
+}
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
-app.use("/", serveStatic(path.resolve(__dirname, "../../dist")));
+app.use('/', serveStatic(path.resolve(__dirname, '../../dist')))
 
-app.get("/api/v1/projects", function(_, res, _) {
+app.get('/api/v1/projects', function(_, res, _) {
   try {
-    res.json({ code: 200, ...data });
+    res.status(404).send('Sorry, we cannot find that!')
+    res.status(500).send({ error: 'something blew up' })
+    res.json({ code: 200, ...data })
   } catch (error) {
-    if ("response" in error) {
-      res.json(error.response.body);
-    } else {
-      res.json(error);
-    }
+    if ('response' in error) {
+      res.json(error.response.body)
+    } else res.json(error)
   }
-});
+})
 
-app.get("/api/v1/projects/:id", function(req, res, _) {
+app.get('/api/v1/projects/:id', function(req, res, _) {
   try {
-    const requestValue = req.params.id;
+    const requestValue = req.params.id
+
+    console.log(req)
 
     const toFind = data.projects.find(project => {
-      return project.id === requestValue;
-    });
+      return project.id === requestValue
+    })
 
     if (toFind) {
-      res.json(toFind);
+      res.json(toFind)
     }
   } catch (error) {
-    if ("response" in error) {
-      res.json(error.response.body);
+    if ('response' in error) {
+      res.json(error.response.body)
     } else {
-      res.json(error);
+      res.json(error)
     }
   }
-});
+})
 
 app.listen(port, () => {
-  console.log(`🚀Server listening on port http://localhost:${port}`);
-});
+  console.log(`🚀Server listening on port http://localhost:${port}`)
+})
